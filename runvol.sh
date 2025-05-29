ad6aef057a939532d018712e2825030e75888164
syaifulandy <syaifulandy@users.noreply.github.com>
2025-05-30 06:48:44 +0700

    Create runvol.sh
    Running volatility dengan beberapa modul

---
+++ runvol.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Mengecek parameter
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 /path/to/memory.raw"
+    exit 1
+fi
+
+MEMFILE="$1"
+MEMBASENAME=$(basename "$MEMFILE")
+OUTDIR="output"
+
+# Membuat folder output jika belum ada
+mkdir -p "$OUTDIR"
+
+# Daftar plugin yang ingin dijalankan
+PLUGINS=(
+  "windows.info"
+  "windows.malfind"
+  "windows.pslist"
+  "windows.pstree"
+  "windows.cmdline"
+  "windows.netstat"
+  "windows.netscan"
+)
+
+# Jalankan tiap plugin dan simpan output ke file
+for plugin in "${PLUGINS[@]}"; do
+  OUTFILE="${OUTDIR}/${plugin//./_}_${MEMBASENAME}.txt"
+  echo "[*] Running $plugin..."
+  vol -q -f "$MEMFILE" "$plugin" > "$OUTFILE" 2>&1
+done
+
+echo "[+] All plugins finished. Output saved to $OUTDIR"
